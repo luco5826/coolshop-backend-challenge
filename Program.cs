3bcfacefa64fb7a415b1c71edd88cf421e2eba17
Luca Errani <luca.errani1@gmail.com>
2022-05-13 09:27:55 +0200

    Add: CSV parser implementation

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿if (args.Length != 3)
+{
+  Console.WriteLine("Usage: dotnet run <target-file> <target-column> <search-key>");
+  return -1;
+}
+
+string targetFile = args[0];
+if (!File.Exists(targetFile))
+{
+  Console.WriteLine("Error: target file does not exists (\"{}\")", targetFile);
+  return -1;
+}
+
+int targetColumn;
+if (!int.TryParse(args[1], out targetColumn))
+{
+  Console.WriteLine("Error: target column must be a number (\"{}\")", targetColumn);
+  return -1;
+}
+
+string searchKey = args[2];
+
+using StreamReader reader = File.OpenText(targetFile);
+
+string? line = null;
+while ((line = reader.ReadLine()) != null)
+{
+  string[] tokens = line.Trim(';').Split(",");
+  if (targetColumn >= tokens.Length)
+  {
+    Console.WriteLine("Error: target column outside available indexes");
+    return -1;
+  }
+  string cell = tokens[targetColumn];
+  if (cell == searchKey)
+  {
+    Console.WriteLine(line);
+  }
+}
+
+return 0;
